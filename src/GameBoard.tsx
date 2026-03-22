f51c681fa600ce6d2207e2b5fbc55ae5f11614f9
Daanyaal Tariq <dtariq1995@gmail.com>
2026-03-22 17:45:25 -0500

    Added useState for cards and API fetch to GameBoard

--- src/GameBoard.tsx
+++ src/GameBoard.tsx
@@ -1,4 +1,13 @@
+import { useState, useEffect } from "react";
+
 function GameBoard() {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    // Fetch card data from Pokemon TCG API and set it to state
+  })
+
+
   return (
     <div className="game-board">
       {/* Game board content goes here */}
